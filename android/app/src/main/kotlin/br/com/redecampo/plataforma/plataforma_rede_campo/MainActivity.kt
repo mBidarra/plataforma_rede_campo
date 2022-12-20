c3e421ad5881162b84316ab4b0b1865561f80d6e
Lucas Evandro Theisen <lucasevandro11@hotmail.com>
2022-12-20 20:02:53 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/br/com/redecampo/plataforma/plataforma_rede_campo/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.redecampo.plataforma.plataforma_rede_campo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter/material.dart';
+import 'package:plataforma_rede_campo/views/home_screen/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Plataforma Rede Campo",
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
