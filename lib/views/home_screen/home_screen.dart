c4fa6dc72874b3d9132ad66f008b575853057381
Lucas Evandro Theisen <78063228+LucasTheisen31@users.noreply.github.com>
2022-12-20 21:03:23 -0300

    Commit 1

--- lib/views/home_screen/home_screen.dart
+++ lib/views/home_screen/home_screen.dart
@@ -1,14 +1,39 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+
     return Scaffold(
       body: ListView(
         children: [
-          
+          PreferredSize(
+            preferredSize: Size(screenSize.width, 1000),
+            child: Container(
+              height: 390,
+              width: screenSize.width,
+              child: Stack(
+                children: [
+                  Image.asset(
+                    'images/Header.png',
+                    fit: BoxFit.contain,
+                  ),
+                  Center(
+                    child: Text(
+                      "Rede Campo",
+                      style: TextStyle(
+                          color: Color.fromRGBO(41, 208, 78, 1),
+                          fontSize: 100,
+                          fontWeight: FontWeight.w700),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
