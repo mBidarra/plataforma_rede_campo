9feeac07f05baa3ffafbde242a2100e2c86a7f4f
Lucas Evandro Theisen <lucasevandro11@hotmail.com>
2022-12-24 14:29:53 -0300

    Commit 1 24/12/22 - cadastro_store

---
+++ lib/stores/cadastro_store.dart
@@ -0,0 +1,118 @@
+import 'package:mobx/mobx.dart';
+import 'package:plataforma_rede_campo/helpers/extensions.dart';
+
+/*Comando queprecisa executar no terminal:
+flutter packages pub run build_runner watch
+flutter pub run build_runner watch --delete-conflicting-outputs
+*/
+
+part 'cadastro_store.g.dart';
+
+class CadastroStore = _CadastroStore with _$CadastroStore;
+
+abstract class _CadastroStore with Store {
+  @observable
+  String? nome = '';
+
+  @action
+  void setNome(String? value) => nome = value;
+
+  @computed
+  bool get nomeValid => nome != null && nome!.length >= 5;
+  String? get nomeError {
+    if (!exibirErros || nomeValid) {
+      return null;
+    } else if (nome!.isEmpty) {
+      return 'Campo obrigatório';
+    } else if (nome!.length < 5) {
+      return "Nome muito curto";
+    } else {
+      return ('Nome invalido');
+    }
+  }
+
+  @observable
+  String? email = '';
+
+  @action
+  void setEmail(String? value) => email = value;
+
+  @computed
+  bool get emailValid =>
+      email == null || email!.isEmpty || email!.isEmailValid();
+  String? get emailError {
+    if (!exibirErros || emailValid) {
+      return null;
+    } else {
+      return 'Email invalido';
+    }
+  }
+
+  @observable
+  String? idadeText = '';
+
+  @action
+  void setIdadeText(String? value) => idadeText = value;
+
+  @computed
+  num? get idade {
+    return num.tryParse(idadeText!);
+  }
+
+  bool get idadeValid => idade != null && idade! > 0 && idade! <= 100;
+  String? get idadeError {
+    if (!exibirErros || idadeValid) {
+      return null;
+    } else if (idadeText!.isEmpty) {
+      return 'Campo obrigatório';
+    } else {
+      return 'Idade inválida';
+    }
+  }
+
+  @observable
+  String? telefone = '';
+
+  @action
+  void setTelefone(String? value) => telefone = value;
+
+  @computed
+  bool get telefoneValid => telefone != null && telefone!.length >= 14;
+
+  String? get telefoneError {
+    if (!exibirErros || telefoneValid) {
+      return null;
+    } else if (telefone!.isEmpty) {
+      return 'Campo obrigatório';
+    } else {
+      return 'Telefone inválido';
+    }
+  }
+
+  @computed
+  bool get isFormValid =>
+      nomeValid && emailValid && idadeValid && telefoneValid;
+
+  @computed
+  dynamic get cadastrarPressed => (isFormValid && !loading) ? _cadastrar : null;
+
+  @observable
+  bool exibirErros = false;
+
+  @action
+  void invalidSendPressed() => exibirErros = true;
+
+  @observable
+  bool loading = false;
+
+  @action
+  void setLoading(bool value) => loading = value;
+
+  Future<void> _cadastrar() async {
+    setLoading(true);
+
+    await Future.delayed(const Duration(seconds: 4));
+
+    setLoading(false);
+  }
+}
--- lib/views/cadastro_screen/cadastro_screen.dart
+++ lib/views/cadastro_screen/cadastro_screen.dart
@@ -3,15 +3,48 @@ import 'package:flutter_svg/svg.dart';
 import 'package:plataforma_rede_campo/components/bottom%20panel/botton%20panel.dart';
 import 'package:plataforma_rede_campo/views/cadastro_screen/components/title_text_form_cadastro.dart';
 
+import '../../components/navigation_bar/navigation_barra.dart';
+
 class CadastroScreen extends StatelessWidget {
   const CadastroScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+
     return Scaffold(
       backgroundColor: Color.fromRGBO(239, 231, 231, 1),
       body: ListView(
         children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("images/header2.png"),
+                fit: BoxFit.cover,
+              ),
+            ),
+            height: 390,
+            width: screenSize.width,
+            child: Stack(
+              children: const [
+                Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    "Rede Campo",
+                    style: TextStyle(
+                        fontFamily: 'Chillax',
+                        color: Color.fromRGBO(41, 208, 78, 1),
+                        fontSize: 100,
+                        fontWeight: FontWeight.w700),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: NavigationBarra(),
+                )
+              ],
+            ),
+          ),
           Padding(
             padding: const EdgeInsets.only(top: 68, left: 55, right: 55),
             child: Row(
@@ -41,10 +74,54 @@ class CadastroScreen extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 SvgPicture.asset('icons/user_circle.svg'),
-                SizedBox(
+                const SizedBox(
                   height: 87,
                 ),
-                TitleTextFormCadastro(title: "Nome completo:"),
+                const TitleTextFormCadastro(title: "Nome completo:"),
+                const SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  style: const TextStyle(fontSize: 25),
+                  //onChanged: loginStore.setPassword,
+                  //enabled: !loginStore.loading,
+                  decoration: InputDecoration(
+                    //errorText: loginStore.passwordError,
+                    filled: true,
+                    fillColor: const Color.fromRGBO(193, 193, 193, 1),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(4),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                  textInputAction: TextInputAction.next,
+                ),
+                const SizedBox(
+                  height: 33,
+                ),
+                const TitleTextFormCadastro(title: "Email:"),
+                const SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  style: const TextStyle(fontSize: 25),
+                  //onChanged: loginStore.setPassword,
+                  //enabled: !loginStore.loading,
+                  decoration: InputDecoration(
+                    //errorText: loginStore.passwordError,
+                    filled: true,
+                    fillColor: const Color.fromRGBO(193, 193, 193, 1),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(4),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                  textInputAction: TextInputAction.next,
+                ),
+                SizedBox(
+                  height: 33,
+                ),
+                const TitleTextFormCadastro(title: "Idade:"),
                 SizedBox(
                   height: 15,
                 ),
@@ -66,51 +143,7 @@ class CadastroScreen extends StatelessWidget {
                 SizedBox(
                   height: 33,
                 ),
-                TitleTextFormCadastro(title: "Email:"),
-                SizedBox(
-                  height: 15,
-                ),
-                TextFormField(
-                  style: const TextStyle(fontSize: 25),
-                  //onChanged: loginStore.setPassword,
-                  //enabled: !loginStore.loading,
-                  decoration: InputDecoration(
-                    //errorText: loginStore.passwordError,
-                    filled: true,
-                    fillColor: const Color.fromRGBO(193, 193, 193, 1),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(4),
-                      borderSide: BorderSide.none,
-                    ),
-                  ),
-                  textInputAction: TextInputAction.next,
-                ),
-                SizedBox(
-                  height: 33,
-                ),
-                TitleTextFormCadastro(title: "Idade:"),
-                SizedBox(
-                  height: 15,
-                ),
-                TextFormField(
-                  style: const TextStyle(fontSize: 25),
-                  //onChanged: loginStore.setPassword,
-                  //enabled: !loginStore.loading,
-                  decoration: InputDecoration(
-                    //errorText: loginStore.passwordError,
-                    filled: true,
-                    fillColor: const Color.fromRGBO(193, 193, 193, 1),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(4),
-                      borderSide: BorderSide.none,
-                    ),
-                  ),
-                  textInputAction: TextInputAction.next,
-                ),
-                SizedBox(
-                  height: 33,
-                ),
-                TitleTextFormCadastro(title: "Telefone:"),
+                const TitleTextFormCadastro(title: "Telefone:"),
                 SizedBox(
                   height: 15,
                 ),
