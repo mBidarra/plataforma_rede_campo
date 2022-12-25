f95fa3d28c4f35c28508597be2af730272601ea3
Lucas Evandro Theisen <lucasevandro11@hotmail.com>
2022-12-25 18:45:38 -0300

    Commit 4 24/12/22 - Home Screen Pesquisador

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 import 'package:plataforma_rede_campo/views/cadastro_screen/cadastro_screen.dart';
 import 'package:plataforma_rede_campo/views/home_screen/home_screen.dart';
 import 'package:plataforma_rede_campo/views/home_screen_pesquisador/home_screen_pesquisador.dart';
@@ -17,9 +18,9 @@ Future<void> main() async {
 }
 
 Future<void> inicializeParse() async {
-  /*const appId = '8MmuFHBcTejklfFhxsvS03gy0XIijosSQp6l5Da8';
+  const appId = 'qcqRQberoTPqvNNhXucLuV9lXnjr99hKSFQYCyBN';
   const serverURL = 'https://parseapi.back4app.com/';
-  const clientKey = 'Jbohcr47krRtH2YnPuusp5xLHvXYH7GNMSmcuRnA';
+  const clientKey = 'cS0ljFgSDhurM8oGaZyO6NoQW4kEUcaa9Y1guGe8';
 
   await Parse().initialize(
     appId,
@@ -27,7 +28,13 @@ Future<void> inicializeParse() async {
     clientKey: clientKey,
     autoSendSessionId: true,
     debug: true,
-  );*/
+  );
+
+  /*final categoria = ParseObject('cat')..set('tit', 'roupa');
+
+  final response = await categoria.save();
+
+  print(response.success);*/
 }
 
 class MyApp extends StatelessWidget {
@@ -35,7 +42,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: "Plataforma Rede Campo",
       debugShowCheckedModeBanner: false,
       home: HomeScreenPesquisador(),
