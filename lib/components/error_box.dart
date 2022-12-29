9d6b63cf7edd94bb664afd7c04626497589899ba
Lucas Evandro Theisen <lucasevandro11@hotmail.com>
2022-12-29 19:49:02 -0300

    Commit 4 29/12/22 - Error Box

---
+++ lib/components/error_box.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ErrorBox extends StatelessWidget {
+  const ErrorBox({Key? key, this.message}) : super(key: key);
+
+  final String? message;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
