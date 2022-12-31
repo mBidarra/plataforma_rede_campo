8f3ac61e0efa0643d43f1a72cf743cf3f2eb6d8a
Lucas Evandro Theisen <lucasevandro11@hotmail.com>
2022-12-30 21:51:01 -0300

    Commit 5 30/12/22 - TableKeys

---
+++ lib/repositories/table_keys.dart
@@ -0,0 +1,39 @@
+const keyId = 'objectId';
+
+//usuario
+const keyUserId = keyId; //id
+const keyUserName = 'name';
+const keyUserEmail = 'email';
+const keyUserPhone = 'phone';
+const keyUserType = 'type';
+const keyUserCreatedAt = 'createdAt';
+const keyUserAuthData = 'authData';
+
+//noticias
+const keyNewsTable = 'News';
+const keyNewsId = keyId; //id
+const keyNewsTitle = 'title';
+const keyNewsDescription = 'description';
+const keyNewsField = 'field'; //campo
+const String keyNewsOwner = 'owner'; //autor
+const keyNewsImage = 'image';
+const keyNewsCreatedAt = 'createdAt';
+const keyNewsUpdatedAt = 'updatedAt';
+
+//projeto
+const String keyProjectTable = 'Project';
+const keyProjectId = keyId; //id
+const keyProjectTitle = 'title';
+const keyProjectDescription = 'description';
+const String keyProjectOwner = 'owner'; //autor
+const String keyProjectCoAuthors = 'coauthors'; //autor
+const keyProjectImage = 'image';
+const keyProjectField = 'field'; //campo
+const keyProjectCreatedAt = 'createdAt';
+const keyProjectUpdatedAt = 'updatedAt';
+
+//campo
+const String keyFieldTable = 'Field';
+const keyFieldId = keyId; //id
+const keyFieldAreaCnpq = 'areaCnpq';
+const keyFieldDescription = 'description';
