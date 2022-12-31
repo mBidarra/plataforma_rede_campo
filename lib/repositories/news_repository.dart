7d8059877a579f17165ad3ed8c4dd74d921896fb
Lucas Evandro Theisen <lucasevandro11@hotmail.com>
2022-12-31 15:37:10 -0300

    Commit 2 31/12/22 - Ajustes Repositories

--- lib/repositories/news_repository.dart
+++ lib/repositories/news_repository.dart
@@ -1,6 +1,6 @@
 import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
+import 'package:plataforma_rede_campo/repositories/parse_errors.dart';
 import 'package:plataforma_rede_campo/repositories/table_keys.dart';
-
 import '../models/news.dart';
 
 class NewsRepository {
@@ -14,6 +14,17 @@ class NewsRepository {
     //verifica se esta sendo editado uma news
     if (news.id != null) {
       newsObject.objectId = news.id;
+    }
+  }
+
+  Future<void> delete(News news) async {
+    final parseObject = ParseObject(keyNewsTable)..set(keyProjectId, news.id);
+
+    //parseObject.set(keyAdStatus, AdStatus.DELETED.index);
+
+    final response = await parseObject.save();
+    if (!response.success) {
+      return Future.error(ParseErrors.getDescription(response.error!.code));
     }
   }
 }
--- lib/repositories/project_repository.dart
+++ lib/repositories/project_repository.dart
@@ -1,5 +1,6 @@
 import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 import 'package:plataforma_rede_campo/models/project.dart';
+import 'package:plataforma_rede_campo/repositories/parse_errors.dart';
 import 'package:plataforma_rede_campo/repositories/table_keys.dart';
 
 class ProjectRepository {
@@ -13,6 +14,17 @@ class ProjectRepository {
     //verifica se esta sendo editado uma news
     if (project.id != null) {
       projectObject.objectId = project.id;
+    }
+  }
+
+  Future<void> delete(Project project) async {
+    final parseObject = ParseObject(keyProjectTable)..set(keyProjectId, project.id);
+
+    //parseObject.set(keyAdStatus, AdStatus.DELETED.index);
+
+    final response = await parseObject.save();
+    if (!response.success) {
+      return Future.error(ParseErrors.getDescription(response.error!.code));
     }
   }
 }
