0d2185e21a08f4f57c1f0ad337c3c4637898a9a9
Lucas Evandro Theisen <lucasevandro11@hotmail.com>
2022-12-31 15:22:26 -0300

    Commit 2 31/12/22 - Ajustes Repositories

--- lib/repositories/news_repository.dart
+++ lib/repositories/news_repository.dart
@@ -1,7 +1,19 @@
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
+import 'package:plataforma_rede_campo/repositories/table_keys.dart';
+
 import '../models/news.dart';
 
 class NewsRepository {
   Future<List<News>> getAllNews() async {
     return Future.error('error');
+  }
+
+  Future<void> saveNews(News news) async {
+    final newsObject = ParseObject(keyNewsTable);
+
+    //verifica se esta sendo editado uma news
+    if (news.id != null) {
+      newsObject.objectId = news.id;
+    }
   }
 }
--- lib/repositories/project_repository.dart
+++ lib/repositories/project_repository.dart
@@ -1,1 +1,18 @@
-class ProjectRepository {}
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
+import 'package:plataforma_rede_campo/models/project.dart';
+import 'package:plataforma_rede_campo/repositories/table_keys.dart';
+
+class ProjectRepository {
+  Future<List<Project>> getAllNews() async {
+    return Future.error('error');
+  }
+
+  Future<void> saveNews(Project project) async {
+    final projectObject = ParseObject(keyProjectTable);
+
+    //verifica se esta sendo editado uma news
+    if (project.id != null) {
+      projectObject.objectId = project.id;
+    }
+  }
+}
