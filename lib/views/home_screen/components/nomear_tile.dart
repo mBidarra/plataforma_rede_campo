76ceefc1d3af3c411e8b7150fcf15249e4e8f3ed
Lucas Evandro Theisen <lucasevandro11@hotmail.com>
2023-01-05 15:17:02 -0300

    Commit 2 05/01/23 Home Screen - News Tile

--- lib/views/home_screen/components/nomear_tile.dart
+++ lib/views/home_screen/components/nomear_tile.dart
@@ -11,13 +11,50 @@ class NomearTile extends StatelessWidget {
     return Container(
       height: 186,
       margin: const EdgeInsets.symmetric(horizontal: 21, vertical: 8),
-      padding: const EdgeInsets.fromLTRB(24, 18, 24, 14),
+      padding: const EdgeInsets.fromLTRB(24, 16, 24, 14),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(20),
         color: Color.fromRGBO(52, 61, 67, 1),
       ),
       child: Column(
-        children: [],
+        //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const Text(
+            "Categoria",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.w600,
+              fontFamily: 'Chillax',
+              color: Color.fromRGBO(134, 205, 47, 1),
+            ),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(
+            "${news.title}",
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.w600,
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(
+            "${news.description}",
+            maxLines: 3,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.w500,
+              color: Color.fromRGBO(255, 255, 255, 0.87),
+            ),
+          ),
+        ],
       ),
     );
   }
